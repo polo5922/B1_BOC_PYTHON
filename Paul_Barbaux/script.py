4fdbef02d2aa70f4d85735b67eb7dd0e8f28a384
polo5922é <paul.barbaux@gmail.com>
2018-12-18 17:20:12 +0100

    name of people add

--- Paul_Barbaux/script.py
+++ Paul_Barbaux/script.py
@@ -1,5 +1,6 @@
 
 import json
+import glob
 
 def get_nb_films():
     jsonFile = json.loads(open('../catalog.json').read())
@@ -8,9 +9,17 @@ def get_nb_films():
         length = length + len(jsonFile[category])
     return length
 
+def get_name_filmotheque():
+    nameList = []
+    for nameString in glob.glob("../filmotheque/*.vu"):
+        newString = nameString.replace("../filmotheque/","")
+        newString = newString.replace(".vu","")
+        nameList.append(newString)
+    return nameList
+
 def main():
     print "Nb total de films dans le catalogue :", get_nb_films()
-
+    print "name : ", ",".join(get_name_filmotheque())
 
 if __name__ == '__main__':
     main()
