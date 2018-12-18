b64656e58a89d1bf406756d0ffab2aa726ec2702
fpaul <frederic.paul@brest-opencampus.com>
2018-12-18 16:54:43 +0100

    update

--- script_fp.py
+++ script_fp.py
@@ -18,8 +18,17 @@ def get_persons_list():
     return ["jessy", "andy"]
 
 
-def get_nb_films(filter=None):
-    return 0
+def get_nb_films(section=None):
+    nbfilms = 0
+    films = json.load(open('./catalog.json'))
+    print "Sections du catalogue: ", films.keys()
+    for section in films.keys():
+        print "Section: ", section
+        section_nb_films = len(films[section])
+        print "Nb films dans la section: ", section_nb_films
+        nbfilms += section_nb_films
+    print "Nb total de films : ", nbfilms
+    return nbfilms
 
 
 def main():
