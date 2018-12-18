b1efd845a460ecddd0fe5bfaa85840160d34fc6f
fpaul <frederic.paul@brest-opencampus.com>
2018-12-18 15:41:50 +0100

    Ajout TODO

--- script_fp.py
+++ script_fp.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 
 import json
+
+
+def get_nb_films():
+    return 0
+
+
+def main():
+    print "Nb total de films dans le catalogue : ", get_nb_films()
+
+
+if __name__ == '__main__':
+    main()
+
